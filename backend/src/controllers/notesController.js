6861abb68eedc70b07005a315abea02b30be23c8
armanbijoy <armanbijoy4@gmail.com>
2025-11-20 01:30:21 +0600

    Schema For Notes Implemented

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,5 +1,12 @@
-export const getAllNotes = (req,res)=>{
-   res.status(200).send('you just fetch  notes')
+import Note from '../models/Note.js'
+export const getAllNotes = async (req,res)=>{
+   try{
+      const notes = await Note.find()
+      res.status(200).json(notes)
+   }catch(err){
+      console.log('Error', err)
+      res.status(500).json({message:'Internernal Server Problem'})
+   }
 }
 
 export const createNotes = (req,res)=>{
