d5b26a39f1711769c3b83c011afe0decf99a3775
armanbijoy <armanbijoy4@gmail.com>
2025-11-20 01:17:55 +0600

    MongoDB Database connection Established

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import noteRoutes from './src/routes/notesRoutes.js'
+import { connectDB } from './src/config/db.js'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
+
+connectDB()
 
 app.use("/api/notes", noteRoutes)
 
@@ -15,3 +20,5 @@ app.use("/api/notes", noteRoutes)
 app.listen(5001,()=>{
     console.log('Server Started at 5001')
 })
+
+
