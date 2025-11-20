aa724e87d3af72b3ff4167c9de7bbaf6e218d411
armanbijoy <armanbijoy4@gmail.com>
2025-11-20 18:02:55 +0600

    React Boilerplat SetUp Complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+import toast from 'react-hot-toast'
+
+const App = () => {
+  return (
+    <div>
+    
+    <button className='text-red-500 p-4 bg-pink-300' onClick={()=>{
+      toast.success("Congrats")
+    }}>Click Me</button>
+
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/create' element={<CreatePage/>}/>
+        <Route path='/note/:id' element={<NoteDetailPage/>}/>
+      </Routes>
+      
+
+    </div>
+  )
+}
+
+export default App
